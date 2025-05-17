2d308fb25259b56fad29aae75332f072f81ff424
ScharizzeAnnBarqueros <annriz311@gmail.com>
2025-05-17 23:52:44 +0800

    A06

--- ADET/A04/assets/js/array.js
+++ ADET/A04/assets/js/array.js
@@ -1,235 +1,68 @@
-var products = [
-    {
-        "category": "Tops",
-        "contents": [
-            {
-                "name": "Indie Aesthetic Lace Halter Top",
-                "code": "IALT",
-                "price": "₱450",
-                "picture": "prod2.webp"
-            },
-            {
-                "name": "Y2K Rhinestone Cross Scarf Top",
-                "code": "YRCST",
-                "price": "₱550",
-                "picture": "prod19.webp"
-            },
-            {
-                "name": "Harajuku Gothic Vintage Tee",
-                "code": "HGVT",
-                "price": "₱470",
-                "picture": "prod4.jpg"
-            },
-            {
-                "name": "Run She Got the Gun Top",
-                "code": "RSGT",
-                "price": "₱400",
-                "picture": "prod13.webp"
-            },
-            {
-                "name": "Y2K Star Mesh Off-Shoulder Long Sleeve Top",
-                "code": "YSMS",
-                "price": "₱600",
-                "picture": "prod14.webp"
-            },
-            {
-                "name": "Guitar Print Ribbed Top",
-                "code": "GPRT",
-                "price": "₱500",
-                "picture": "prod11.webp"
-            },
-            {
-                "name": "Vinyl Print Crop Top",
-                "code": "VPCT",
-                "price": "₱400",
-                "picture": "prod5.jpg"
-            }
-            ,
-            {
-                "name": "Y2K Eyes Graphic Shirt",
-                "code": "YEGS",
-                "price": "₱480",
-                "picture": "prod3.jpg"
-            },
-            {
-                "name": "Star Print Ribbed Long Sleeve Top",
-                "code": "SPRLST",
-                "price": "₱530",
-                "picture": "prod10.webp"
-            }
-
-        ]
-    },
-
-    {
-        "category": "Pants ",
-        "contents": [
-            {
-                "name": "Grunge Aesthetic Ripped Wide Jeans",
-                "code": "GARWJ",
-                "price": "₱1,200",
-                "picture": "prod15.webp"
-            },
-            {
-                "name": "Japanese Dragon Aesthetic Jeans",
-                "code": "JDAJ",
-                "price": "₱1,200",
-                "picture": "prod9.webp"
-            },
-            {
-                "name": "Grunge Aesthetic Knee Buckle Jeans",
-                "code": "GAKBJ",
-                "price": "₱1,150",
-                "picture": "prod12.webp"
-            },
-            {
-                "name": "90's Kids Jeans in Washed Brown",
-                "code": "9KJWB",
-                "price": "₱950",
-                "picture": "prod8.webp"
-            }
+var products = [];
+var categories = [];
 
 
-        ]
-    },
+const getAllCategories = async () => {
+  fetch(
+      'http://localhost/ScharizzeAnnBarqueros.github.io/ADET/A06/categories.php'
+      )
+    .then(response => response.json())
+    .then(data => {
+      categories = data;
+      loadCategories();
+    });
+}
 
-    {
-        "category": "Boots ",
-        "contents": [
-            {
-                "name": "Love Bites Boots",
-                "code": "LBB",
-                "price": "₱1,700",
-                "picture": "prod16.webp"
-            },
-            {
-                "name": "Platform Ankle Boots",
-                "code": "PAB",
-                "price": "₱1,800",
-                "picture": "prod18.webp"
-            },
-            {
-                "name": "Lebanon Ankle Boots",
-                "code": "LAB",
-                "price": "₱1,600",
-                "picture": "prod17.webp"
-            }
+const getAllProducts = async (categoryID) => {
 
-        ]
-    },
+  fetch(
+      'http://localhost/ScharizzeAnnBarqueros.github.io/ADET/A06/products.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ categoryID })
+      })
+    .then(response => response.json())
+    .then(data => {
+      products = data;
+      loadProducts();
+    });
+}
 
-    {
-        "category": "Accessories  ",
-        "contents": [
-            {
-                "name": "Stars Ring",
-                "code": "SR",
-                "price": "₱200",
-                "picture": "prod24.webp"
-            },
-            {
-                "name": "Band-Aid Nose Clip Jewelry",
-                "code": "BACJ",
-                "price": "₱120",
-                "picture": "prod22.webp"
-            },
-            {
-                "name": "Grunge Heart Chain Necklace",
-                "code": "GHCN",
-                "price": "₱350",
-                "picture": "prod25.webp"
-            }
-            ,
-            {
-                "name": "Grunge Bear Heart Earrings",
-                "code": "GBHE",
-                "price": "₱250",
-                "picture": "prod20.webp"
-            }
-            ,
-            {
-                "name": "Grunge Aesthetic Star Leather Bracelet",
-                "code": "GASLB",
-                "price": "₱300",
-                "picture": "prod23.webp"
-            },
-            {
-                "name": "Silver Star and Crystal Open Bracelet",
-                "code": "SSCOB",
-                "price": "₱250",
-                "picture": "prod1.webp"
-            }
-
-        ]
-    },
-
-    {
-        "category": "Outerwear  ",
-        "contents": [
-
-            {
-                "name": "Come As You Are Grunge Leather Jacket",
-                "code": "CAYAGLJ",
-                "price": "₱1,800",
-                "picture": "prod6.webp"
-            },
-            {
-                "name": "80's Grandma Striped Cardigan Sweater",
-                "code": "8GSCS",
-                "price": "₱650",
-                "picture": "prod7.webp"
-            }
-
-
-
-        ]
-    }, {
-        "category": "Bag  ",
-        "contents": [
-
-            {
-                "name": "Dark Washed Hobo Bag",
-                "code": "DWHB",
-                "price": "₱950",
-                "picture": "prod21.webp"
-            }
-
-        ]
-    }
-]
-
+getAllCategories();
 
 var total = 0;
 
 function loadCategories() {
     var categoriesContainer = document.getElementById("categories");
 
-    products.forEach(function (product, index) {
+    categories.forEach(function (category) {
         categoriesContainer.innerHTML +=
-            `<div onclick="loadProducts('` + index + `')" class="button-category p-3 m-2">
-                <div class="category-name px-3">` + product.category + `</div>
+            `<div onclick="getAllProducts('` + category.categoryID + `')" class="button-category p-3 m-2">
+                <div class="category-name px-3">` + category.name + `</div>
             </div>
             `;
 
     });
 }
 
-function loadProducts(categoryIndex) {
+function loadProducts(categoryID) {
     var maincontainer = document.getElementById("maincontainer");
     maincontainer.innerHTML = "";
 
-    products[categoryIndex].contents.forEach(content => {
+    products.forEach(product => {
         maincontainer.innerHTML += `
-<div onclick="addToReceipt ('`+ content.code + `','` + content.price + `')" class="product rounded-3" >
+<div onclick="addToReceipt ('`+ product.code + `','` + product.price + `')" class="product rounded-3" >
                 <div class="card-content p-2">
-                    <img src="assets/img/` + content.picture + `">
+                    <img src="assets/img/` + product.picture + `">
                     <div class="card-text pt-2">
                       <div class="product-name">
-                      ` + content.name + `
+                      ` + product.name + `
                       </div>
-                      <div class="product-price">
-                         ` + content.price + `
-                      </div>
+                      <span class="product-price">
+                         ₱` + product.price + `
+                      </span>
                     </div>
                   </div>
                   </div>
@@ -254,7 +87,7 @@ function addToReceipt(code, price) {
     
       <div class="d-flex justify-content-between align-items-center px-2">
         <div><small>`+ code + `</small></div>
-        <div><small>`+ price + `</small></div>
+        <span><small>₱`+ price + `</small></span>
       </div>
     `;
 }
---
+++ ADET/A06/pos.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 17, 2025 at 05:44 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `categoryID` int(4) NOT NULL,
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`categoryID`, `name`) VALUES
+(1, 'Tops'),
+(2, 'Pants'),
+(3, 'Boots'),
+(4, 'Accessories'),
+(5, 'Outerwear'),
+(6, 'Bag');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `productID` int(4) NOT NULL,
+  `categoryID` int(4) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `code` varchar(10) NOT NULL,
+  `price` int(4) NOT NULL,
+  `picture` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`productID`, `categoryID`, `name`, `code`, `price`, `picture`) VALUES
+(1, 1, 'Indie Aesthetic Lace Halter Top', 'IALT', 450, 'prod2.webp'),
+(2, 1, 'Harajuku Gothic Vintage Tee', 'HGVT', 470, 'prod4.jpg'),
+(3, 1, 'Run She Got the Gun Top', 'RSGT', 400, 'prod13.webp'),
+(4, 1, 'Y2K Star Mesh Off-Shoulder Long Sleeve Top', 'YSMS', 600, 'prod14.webp'),
+(5, 1, 'Guitar Print Ribbed Top', 'GPRT', 500, 'prod11.webp'),
+(6, 1, 'Vinyl Print Crop Top', 'VPCT', 400, 'prod5.jpg'),
+(7, 1, 'Y2K Eyes Graphic Shirt', 'YEGS', 480, 'prod3.jpg'),
+(8, 1, 'Star Print Ribbed Long Sleeve Top', 'SPRLST', 530, 'prod10.webp'),
+(9, 2, 'Grunge Aesthetic Ripped Wide Jeans', 'GARWJ', 1200, 'prod15.webp'),
+(10, 2, 'Japanese Dragon Aesthetic Jeans', 'JDAJ', 1200, 'prod9.webp'),
+(11, 2, 'Grunge Aesthetic Knee Buckle Jeans', 'GAKBJ', 1150, 'prod12.webp'),
+(12, 2, '90\'s Kids Jeans in Washed Brown', '9KJWB', 950, 'prod8.webp'),
+(13, 3, 'Love Bites Boots', 'LBB', 1700, 'prod16.webp'),
+(14, 3, 'Platform Ankle Boots', 'PAB', 1800, 'prod18.webp'),
+(15, 3, 'Lebanon Ankle Boots', 'LAB', 1600, 'prod17.webp'),
+(16, 4, 'Stars Ring', 'SR', 200, 'prod24.webp'),
+(17, 4, 'Band-Aid Nose Clip Jewelry', 'BACJ', 120, 'prod12.webp'),
+(18, 4, 'Grunge Bear Heart Earrings', 'GBHE', 250, 'prod20.webp'),
+(19, 4, 'Grunge Aesthetic Star Leather Bracelet', 'GASLB', 300, 'prod23.webp'),
+(20, 4, 'Silver Star and Crystal Open Bracelet', 'SSCOB', 250, 'prod1.webp'),
+(21, 5, 'Come As You Are Grunge Leather Jacket', 'CAYAGLJ', 1800, 'prod6.webp'),
+(22, 5, '80\'s Grandma Striped Cardigan Sweater', '8GSCS', 650, 'prod7.webp'),
+(23, 6, 'Dark Washed Hobo Bag', 'DWHB', 950, 'prod21.webp');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`categoryID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`productID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `categoryID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `productID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
